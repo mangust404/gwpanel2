f788b288aa91511d0d6cae5de322322846260280
Riki_tiki_tavi <mangust404@gmail.com>
2014-09-16 19:30:02 +0600

    syntax error

--- example/example_widgets.js
+++ example/example_widgets.js
@@ -8,17 +8,17 @@
     ///
   }
   
-jQuery.extend(panel, {
-  example_widget: function(options) {
-    /// в этой функции this указывает на jQuery-объект созданного виджета
-    /// здесь вы должны осуществить прорисовку виджета
-    /// CSS для виджета должен храниться в теме, в соответствующей папке
-    panel.loadCSS('example/widget.css');
-    /// чтобы передать this в низлежащие области видимости, можно сделать вот так:
-    var that = this;
-    jQuery.each(options, function(key, value)) {
-      that.append('<p>' + key + '=' + value + '</p>');
-    });
-  }
-});
+  jQuery.extend(panel, {
+    example_widget: function(options) {
+      /// в этой функции this указывает на jQuery-объект созданного виджета
+      /// здесь вы должны осуществить прорисовку виджета
+      /// CSS для виджета должен храниться в теме, в соответствующей папке
+      panel.loadCSS('example/widget.css');
+      /// чтобы передать this в низлежащие области видимости, можно сделать вот так:
+      var that = this;
+      jQuery.each(options, function(key, value) {
+        that.append('<p>' + key + '=' + value + '</p>');
+      });
+    }
+  });
 })(window.__panel);
