1156259f7f7aa5614d78f5976030bc86460f9591
Riki_tiki_tavi <mangust404@gmail.com>
2014-09-21 00:43:42 +0600

    чутка поменял

--- player/player_casino.js
+++ player/player_casino.js
@@ -8,18 +8,24 @@ var gMoney = {
 
 jQuery.extend(panel, {
   player_casino: function(options) {
-    var labelTextOfMoney;
-
     if(jQuery('body:contains("Internal error")').length) return;                              // не перс стерт - выходим
 
+    var seekParams = {
+      roulette: ["Потрачено в казино", "Выигрыш в казино", "Разница в казино"],
+      total: ["Потрачено в тотализаторе", "Выигрыш в тотализаторе", "Разница в тотализаторе"],
+      poker: ["Потрачено на покер", "Получено с покера", "Разница в покере"]
+    };
     if(options.roulette || options.total || options.poker){                              // какая-то инфа будет показана
       jQuery('b:contains("Статистика")').closest('tr').next().find('td:last').find('b').each(
         function(){
-          labelTextOfMoney = this.previousSibling;
-          if(labelTextOfMoney != null){
-            parseAndInsert(options.roulette, this, labelTextOfMoney, "Потрачено в казино", "Выигрыш в казино", "Разница в казино");
-            parseAndInsert(options.total, this, labelTextOfMoney, "Потрачено в тотализаторе", "Выигрыш в тотализаторе", "Разница в тотализаторе");
-            parseAndInsert(options.poker, this, labelTextOfMoney, "Потрачено на покер", "Получено с покера", "Разница в покере");
+          if(this.previousSibling){
+            for(var param in seekParams) {
+              if(parseAndInsert(options[param], this, this.previousSibling, 
+                seekParams[param][0], seekParams[param][1], seekParams[param][2])) {
+                seekParams[param] = null;
+                break;
+              }
+            }
           }
         }
       );
@@ -36,6 +42,7 @@ function parseAndInsert(key, object, labelTextOfMoney, stringIn, stringOut, stri
       gMoney.out = panel.convertingMoneyToInt(jQuery(object).text());
       gMoney.difference = gMoney.out - gMoney.in;
       jQuery(object).next().after("&nbsp;&nbsp;" + stringDifference + ": <b>" + panel.convertingIntToMoney(gMoney.difference) + "</b><br>");
+      return true;
     }
   }
 }
