bd73c1fdbfeb62d17fdca13aa3500c6d6d47c0e4
Riki_tiki_tavi <mangust404@gmail.com>
2014-09-20 14:28:46 +0600

    Доработан модуль-пример, добавлен тест одной функции

---
+++ example/example_test.js
@@ -0,0 +1,72 @@
+QUnit.module('example');
+
+/**
+* Тестирование - очень важный момент при разработке модулей.
+* Поскольку в игре постоянно что-то меняется: разметка, переименовываются страницы,
+* добавляются новые данные на страницах. В том случае, если ваш модуль добавляет
+* какие-то данные на страницы, или получает их, то при изменении в игре ваш модуль
+* может отвалиться. Чтобы узнать, что ваш модуль отвалился, и нужны эти тесты.
+*
+* Основной метод тестирования - открытие нового фрейма с соответствующей страницей,
+* ожидание пока панель вызовет вашу функцию, и проверка видимого результата.
+*
+* Если ваш модуль предоставляет какую-то функцию, которая может использоваться другими 
+* модулями, то также нужно написать тест для этих функций
+*/ 
+
+QUnit.asyncTest('Проверка подсветки ссылок', function(assert) {
+  /// открываем домашнюю страничку
+  /// параметр в конце gwpanel_test=true&continue говорит гвпанели открывать страницу
+  /// в соответствующем тестовом окружении
+  $('<iframe src="http://www.ganjawars.ru/me/?gwpanel_test=true&continue"></iframe>').load(function() {
+    /// запоминаем окно iframe-а в переменную, чтобы иметь к ней доступ
+    var __window = this.contentWindow;
+    waitPanelInitialization(this.contentWindow, function() {
+      // эта функция вызывается когда панель подгрузилась и прорисовалась
+
+      // обращаемся к jQuery открытого iframe-а
+      // и запрашиваем есть ли ссылки с классом "player-info"
+      assert.ok(__window.jQuery('a.player-info').length > 0, 'Ссылки подсветились');
+
+      // эту функцию нужно вызвать по завершении теста
+      QUnit.start();
+    });
+  }).appendTo('#qunit-fixture');
+});
+
+QUnit.asyncTest('Проверка при наведении на ссылки', function(assert) {
+  $('<iframe src="http://www.ganjawars.ru/me/?gwpanel_test=true&continue"></iframe>').load(function() {
+    /// запоминаем окно iframe-а в переменную, чтобы иметь к ней доступ
+    var __window = this.contentWindow;
+    waitPanelInitialization(this.contentWindow, function() {
+      // получаем первую попавшуюся ссылку
+      var $link = __window.jQuery('a.player-info:visible:first');
+      assert.equal($link.length, 1, 'Найдена ссылка на персонажа');
+
+      // эмулируем наведение на ссылку
+      $link.mouseenter();
+
+      // Поскольку мы не можем точно сказать когда данные будут подгружены
+      // и окошко с данными игрока прорисуются, то используем метод waitFor.
+      // Этот метод вызывает функцию в первом агрументе до тех пор, пока она
+      // не вернёт true,  либо не пройдёт таймаут (по-умолчанию 10 секунд), 
+      // и после того как первая функция вернёт true, вызывается вторая функция
+      waitFor(function() {
+        return __window.jQuery('.player-info-div:visible').length > 0;
+      }, function() {
+        var $player_info_div = __window.jQuery('.player-info-div:visible');
+
+        assert.ok($player_info_div.text().search(/Боевой: [0-9]+/), 
+                    'Выведен боевой уровень');
+        assert.ok($player_info_div.text().search(/Экономический: [0-9]+/), 
+                    'Выведен экономический уровень');
+        assert.ok($player_info_div.text().search(/Производственный: [0-9]+/), 
+                    'Выведен производственный уровень');
+        QUnit.start();
+      });
+    });
+  }).css({width: 1000, height: 600}).appendTo('#qunit-fixture');
+
+  // Если нужно посмотреть, что происходит во фрейме, можно использовать такой код
+  //$('#qunit-fixture').css({height: 1000, width: 1000, position: 'static'}).show();
+});
