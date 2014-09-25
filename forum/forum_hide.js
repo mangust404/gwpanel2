f36c802b05353ae57f5cea9142f62b2d3464f3bb
Breein <grig.cem@gmail.com>
2014-09-26 00:37:34 +0700

    Исправил код модуля по рекомендациям.

--- forum/forum_hide.js
+++ forum/forum_hide.js
@@ -1,11 +1,12 @@
 (function(panel) {
-  var forumsDefine = {
-    "main_forums" : {"count": 16, "header": "Основные форумы"},
-    "trade_forums": {"count": 12, "header": "Торговля"},
-    "sud_forums"  : {"count": 4, "header": "Суд"},
-    "tech_forums" : {"count": 5, "header": "Технические вопросы"},
-    "rh_forums"   : {"count": 1, "header": "Автономные районы"}
-  };
+
+var forumsDefine = {
+  "main_forums" : {"count": 16, "header": "Основные форумы"},
+  "trade_forums": {"count": 12, "header": "Торговля"},
+  "sud_forums"  : {"count": 4, "header": "Суд"},
+  "tech_forums" : {"count": 5, "header": "Технические вопросы"},
+  "rh_forums"   : {"count": 1, "header": "Автономные районы"}
+};
 
 jQuery.extend(panel, {
   forum_hide: function(options){
@@ -17,15 +18,15 @@ jQuery.extend(panel, {
     if(location.search != "?gid=2"){
       $forumsTable = jQuery('b:contains("Основные форумы")').closest('table');
       $tempForums = jQuery('<table>').html($forumsTable.html());
-
-      counter = 0;
+      counter = {"all": 0, "success": 0};
 
       for(i in options){
-        if(typeof options[i] == "object"){
-          counter += hideForum($tempForums, i, options[i]);
+        if(jQuery.type(options[i]) == 'array'){
+          counter.all++;
+          counter.success += hideForum($tempForums, i, options[i]);
         }
       }
-      if(counter == 5){
+      if(counter.all == counter.success){
         $forumsTable.html($tempForums.html());
       }
     } else {
@@ -39,28 +40,28 @@ jQuery.extend(panel, {
   }
 });
 
-  function hideForum($tempList, section, dataSection){
-    var i, length;
-    var $forumHeader, $tip;
-    length = dataSection.length;
+function hideForum($tempList, section, dataSection){
+  var i, length;
+  var $forumHeader;
+  length = dataSection.length;
 
-    if(length){
-      $forumHeader = $tempList.find('b:contains("' + forumsDefine[section].header + '")');
+  if(!length) return 1;
+  $forumHeader = $tempList.find('b:contains("' + forumsDefine[section].header + '")');
 
-      for(i = 0; i < length; i++){
-        console.log(dataSection[i]);
-        $tempList.find('a[href="/threads.php?' + dataSection[i] + '"]').closest('tr').remove();
-      }
-      if(length == forumsDefine[section].count){
-        $forumHeader.closest('tr').remove();
-      } else {
-        $tip = jQuery("<div>").html(" Скрыто форумов: " + i).addClass('tip_forumHeader');
-        $tip.prop("title", "Их номера: " + dataSection.join(", "));
-        $forumHeader.addClass('def_forumHeader');
-        $forumHeader.after($tip);
-      }
-    }
-
-    return 1;
+  for(i = 0; i < length; i++){
+    $tempList.find('a[href="/threads.php?' + dataSection[i] + '"]').closest('tr').remove();
   }
+  if(length == forumsDefine[section].count){
+    $forumHeader.closest('tr').remove();
+  } else {
+    $forumHeader.addClass('def_forumHeader');
+    jQuery("<div>")
+      .html("Скрыто разделов: " + i)
+      .addClass('tip_forumHeader')
+      .attr("title", "Их номера: " + dataSection.join(", "))
+      .insertAfter($forumHeader);
+  }
+
+  return 1;
+}
 })(window.__panel);
