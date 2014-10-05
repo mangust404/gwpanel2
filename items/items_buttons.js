cce393cc238db1feac5d5484e185a56e12176f55
Riki_tiki_tavi <mangust404@gmail.com>
2014-10-05 21:47:43 +0600

    кнопка "снять всё"

--- items/items_buttons.js
+++ items/items_buttons.js
@@ -1,21 +1,45 @@
 (function(panel) {
 
-  jQuery.extend(panel, {
+  $.extend(panel, {
     items_putset: function(options) {
       var that = this;
-      jQuery.ajax('http://www.ganjawars.ru/home.do.php?putset=' + options.set_id, {
+      $.ajax('http://www.ganjawars.ru/home.do.php?putset=' + options.set_id, {
         success: function(data) {
           /// Проверяем есть ли ссылка на этот комплект на странице
-          if(jQuery(data).find('a[href*="/home.do.php?putset=' + options.set_id + '"]').length > 0) {
-            jQuery(that).addClass('button-ok');
+          if($(data).find('a[href*="/home.do.php?putset=' + options.set_id + '"]').length > 0) {
+            $(that).addClass('button-ok');
           } else {
-            jQuery(that).addClass('button-error');
+            $(that).addClass('button-error');
           }
         }, 
         error: function() {
-          jQuery(that).addClass('button-error');
+          $(that).addClass('button-error');
+        }
+      });
+    },
+    items_undress: function() {
+      var that = this;
+      $.ajax('http://www.ganjawars.ru/items.php', {
+        success: function(data) {
+          /// Проверяем есть ли ссылка на этот комплект на странице
+          var link = $(data).find('a[href*="/home.do.php?dress_off="]');
+          if(link.length > 0) {
+            $.ajax(link.attr('href'), {
+              success: function() {
+                $(that).addClass('button-ok');
+              },
+              error: function() {
+                $(that).addClass('button-error');
+              }
+            });
+          } else {
+            $(that).addClass('button-error');
+          }
+        }, 
+        error: function() {
+          $(that).addClass('button-error');
         }
       });
     }
   });
-})(window.__panel);
+})(window.__panel, jQuery);
