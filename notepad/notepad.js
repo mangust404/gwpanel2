a318eda9d1746bce749a3ab5f3477a4c438f3b91
Riki_tiki_tavi <mangust404@gmail.com>
2014-10-12 09:50:57 +0600

    Блокнот + кнопка блокнота

--- notepad/notepad.js
+++ notepad/notepad.js
@@ -1,7 +1,21 @@
 (function(panel, $) {
+  var $notepad;
 jQuery.extend(panel, {
   notepad_button: function(options) {
-    alert('ещё не готово');
+    panel.loadCSS('notepad/notepad.css');
+    panel.get('notepad', function(data) {
+      if(!$notepad) {
+        $notepad = $('<div>', {id: 'notepad'}).appendTo(document.body);
+        var $container = $('<div>', {class: 'container'}).appendTo($notepad);
+        $('<textarea>').val(data).appendTo($container);
+        $('<button>', {class: 'close-button'}).html('Сохранить и закрыть').click(function() {
+          $notepad.fadeOut();
+          panel.set('notepad', $notepad.find('textarea').val());
+        }).appendTo($notepad);
+      }
+      $notepad.fadeIn();
+      $notepad.find('textarea').focus();
+    });
   }
   
 });
