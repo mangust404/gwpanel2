bd0b67d9f2ef35ff894b1be16804bc4182a08ba7
Riki_tiki_tavi <mangust404@gmail.com>
2014-10-12 09:59:55 +0600

    блокнот отдельный для каждого перса

--- notepad/notepad.js
+++ notepad/notepad.js
@@ -3,14 +3,14 @@
 jQuery.extend(panel, {
   notepad_button: function(options) {
     panel.loadCSS('notepad/notepad.css');
-    panel.get('notepad', function(data) {
+    panel.get('notepad_' + panel.currentPlayerID(), function(data) {
       if(!$notepad) {
         $notepad = $('<div>', {id: 'notepad'}).appendTo(document.body);
         var $container = $('<div>', {class: 'container'}).appendTo($notepad);
         $('<textarea>').val(data).appendTo($container);
         $('<button>', {class: 'close-button'}).html('Сохранить и закрыть').click(function() {
           $notepad.fadeOut();
-          panel.set('notepad', $notepad.find('textarea').val());
+          panel.set('notepad_' + panel.currentPlayerID(), $notepad.find('textarea').val());
         }).appendTo($notepad);
       }
       $notepad.fadeIn();
