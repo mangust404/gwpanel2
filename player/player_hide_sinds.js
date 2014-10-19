e423bb6d08c7e6c896200cfc1e141e0d17c2e79d
Breein <grig.cem@gmail.com>
2014-10-19 20:48:29 +0700

    Заюзал новый метод для иконки. Заменил jQuery на $.

--- player/player_hide_sinds.js
+++ player/player_hide_sinds.js
@@ -1,19 +1,19 @@
 // author   гном убийца
 // id       433067
 
-(function(panel) {
-  jQuery.extend(panel, {
+(function(panel, $) {
+  $.extend(panel, {
     player_hide_sinds: function(options){
       var $td, $contentTd, $newContent;
       var index, i, length, html, url;
 
-      $td = jQuery('b:contains("Синдикаты")').closest('tr').next().children();
+      $td = $('b:contains("Синдикаты")').closest('tr').next().children();
       $contentTd = $td.children();
 
       if($contentTd.eq(0).is('br') && $contentTd.eq(1).is('br')) return false;
       if($td.text().search("остальных синдикатов скрыт") != -1) return false;
 
-      $newContent = jQuery('<span>').append('<br>');
+      $newContent = $('<span>').append('<br>');
       index = 1;
       html = "";
       panel.loadCSS("player/player_hide_sinds.css");
@@ -35,33 +35,32 @@
       }
 
       $newContent.append(
-        jQuery('<div>').addClass("center spoiler").append(
-          jQuery('<div>').addClass("title").html(
-            '&nbsp;&nbsp;<img src="' + panel.path_to_theme() + 'icons/spoiler_open.png" />&nbsp;Список остальных синдикатов'
+        $('<div>').addClass("center spoiler").append(
+          $('<div>').addClass("title").html(
+            '&nbsp;&nbsp;<img src="'+ panel.iconURL('spoiler_open.png') +'" />&nbsp;Список остальных синдикатов'
           ).click(
             function(){
               var $content, $img;
-              $content = jQuery("#listContent");
-              $img = jQuery(this).find('img');
+              $content = $("#listContent");
+              $img = $(this).find('img');
 
               if($content.is(':hidden')){
                 $content.show();
-                $img.prop('src', panel.path_to_theme() + 'icons/spoiler_close.png');
+                $img.prop('src', panel.iconURL('spoiler_close.png'));
               } else {
                 $content.hide();
-                $img.prop('src', panel.path_to_theme() + 'icons/spoiler_open.png');
+                $img.prop('src', panel.iconURL('spoiler_open.png'));
               }
             }
           )
         ).append(
-          jQuery('<div>').addClass("content").html(html).hide().prop('id', 'listContent')
+          $('<div>').addClass("content").html(html).hide().prop('id', 'listContent')
         ).append(
-          jQuery('<div>').addClass("space")
+          $('<div>').addClass("space")
         )
-      ).append(jQuery('<br>'));
+      ).append($('<br>'));
 
       $td.empty().append($newContent);
-
     }
   });
-})(window.__panel);
+})(window.__panel, jQuery);
