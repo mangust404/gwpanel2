61cf2f7db1b65911aed10af534a0a1cfa35a5812
Riki_tiki_tavi <mangust404@gmail.com>
2014-11-07 00:53:46 +0600

    исправлена ошибка таймера когда производятся полностью аяксовые переходы

--- map/map.js
+++ map/map.js
@@ -1,24 +1,40 @@
 (function(panel, $) {
+
+  function formatTime(s) {
+    var h = Math.floor(s / 3600);
+    var m = Math.floor((s - h * 3600) / 60);
+    var s = s - h * 3600 - m * 60;
+    return (h > 0? (h < 9? '0' + h: h) + ':': '') + (m < 10? '0' + m: m) + ':' + (s < 10? '0' + s: s);
+  }
+  
 jQuery.extend(panel, {
   map_mmupdate: function(event) {
+
     if($('table[background*="/i/map"]').length > 0) {
       panel.showFlash('Ошибка! Скорее всего у вас нет GPS-навигатора и ваш транспорт не оборудован им, либо вы в заявке на бой.');
     }
     //Перезаписываем функцию игры для обновления таймера при передвижении по секторам
     window.mmupdate = function() {
-      if (window.mmtimer>=0) clearTimeout(window.mmtimer);
-      window.mmtimer = panel.setTimeout('mmupdate()',1000);
-      if(!window.arriveTime) {
+      if (window.mmtimer >= 0) {
+        clearTimeout(window.mmtimer);
+        window.mmtimer = 0;
+      }
+      window.mmtimer = setTimeout('mmupdate()', 1000);
+      if (!window.arriveTime) {
         var d = new Date;
-        window.arriveTime = parseInt(d.getTime() / 1000) + timeleft - 1;
+        window.arriveTime = parseInt(d.getTime() / 1000) + window.timeleft - 1;
       };
       var d = new Date;
       window.timeleft = window.arriveTime - parseInt(d.getTime() / 1000);
-      if (window.timeleft<0) window.timeleft=0;
-      $('#mmdiv').html(panel.map_formatTime(timeleft));
+      if (window.timeleft < 0) {
+        window.timeleft = 0;
+        window.arriveTime = 0;
+      }
+      $('#mmdiv').html(formatTime(timeleft));
     };
-    
+
     panel.get('moveHref', function(moveHref) {
+
       if(moveHref && moveHref.length) {
         if($('#gw-content').length) {
           var $target = $('#gw-content');
@@ -42,8 +58,11 @@ jQuery.extend(panel, {
           });
         };
       }
+
     });
+
     panel.loadScript('map/map_sectors.js', function() {
+
       if($(document.body).html().match(/в <nobr><b>([^<]+)<\/b>/)) {
         var sector = RegExp.$1;
         for(var key in panel.map_names) {
@@ -53,31 +72,30 @@ jQuery.extend(panel, {
           }
         }
       }
+
     });
-    
+
   },
   
   map_sector: function() {
     $(function() {
+
       if($(document.body).html().match(/Район: <a href=['"]*\/map\.php\?sx=([0-9]+)[^=]+sy=([0-9]+)/)) {
         //alert(RegExp.$1 + 'x' + RegExp.$2);
         panel.set('map_sector', RegExp.$1 + 'x' + RegExp.$2);
       }
+
     });
-  },
-  
-  map_formatTime: function(s) {
-    var h = Math.floor(s / 3600);
-    var m = Math.floor((s - h * 3600) / 60);
-    var s = s - h * 3600 - m * 60;
-    return (h > 0? (h < 9? '0' + h: h) + ':': '') + (m < 10? '0' + m: m) + ':' + (s < 10? '0' + s: s);
   },
   
   map_links: function() {
     $('a[href*="map.php?sx="]:not(.fast-move):visible').filter(function() {
+
       if($(this).parents('.pane').length > 0) return false;
       return true;
+
     }).addClass('fast-move').addClass('ajax').each(function() {
+
       var matches = $(this).attr('href').match(/sx=([0-9]+)&sy=([0-9]+)$/);
       if(!matches) return;
       var $a = $('<a>[&raquo;]</a>', {
@@ -95,10 +113,12 @@ jQuery.extend(panel, {
         return false;
       });
 
-      if(location.pathname == '/statlist.php')
+      if(location.pathname == '/statlist.php') {
         a.appendTo(this.parentNode);
-      else 
+      } else {
         $(this).after($a);
+      }
+
     });
   },
 
