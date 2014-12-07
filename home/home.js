a05a66e7db0a31f4924a42a45e5d5bd28a8ade7a
Riki_tiki_tavi <mangust404@gmail.com>
2014-12-07 22:03:20 +0600

    добавлен вывод апгрейдов

--- home/home.js
+++ home/home.js
@@ -27,6 +27,9 @@ jQuery.extend(panel, {
         $items.each(function() {
           var ar = this.href.split('item_id=')[1].split('&');
           var item_id = ar[0];
+          var upgrade;
+          var upgInfo;
+
           if(ar[1]) {
             var mod_ar = ar[1].split('=');
             if(mod_ar[0] == 'm' && parseInt(mod_ar[1]) > 0) {
@@ -52,6 +55,56 @@ jQuery.extend(panel, {
                   .appendTo(this.parentNode);
               }
             }
+
+            if(mod_ar[0] == 'upg' && parseInt(mod_ar[1]) > 0) {
+              upgrade = mod_ar[1];
+            }
+          }
+
+          if(ar[2]) {
+            var mod_ar = ar[2].split('=');
+            if(mod_ar[0] == 'upg' && parseInt(mod_ar[1]) > 0) {
+              upgrade = mod_ar[1];
+            }
+          }
+
+          switch(upgrade) {
+            case '1': upgInfo = ['D', 'увеличена дальность']; break;
+            case '2': upgInfo = ['P', 'увеличен урон']; break;
+            case '3': upgInfo = ['S', 'увеличено количество выстрелов']; break;
+            case '10': upgInfo = ['D', 'увеличена броня']; break;
+            case '11': upgInfo = ['A', 'увеличена активная броня']; break;
+            case '12': 
+              upgInfo = ['B']; 
+              switch(items[item_id].type) {
+                case 'helmets': upgInfo.push('бонус снайпера'); break;
+                case 'armour': upgInfo.push('крепкий орешек'); break;
+                case 'boots': upgInfo.push('второй шаг'); break;
+              }
+            break;
+            case '13': 
+              upgInfo = ['P']; 
+              switch(items[item_id].type) {
+                case 'helmets': upgInfo.push('увеличена меткость'); break;
+                case 'armour': upgInfo.push('увеличена выносливость'); break;
+                case 'boots': upgInfo.push('увеличена сила'); break;
+              }
+            break;
+          }
+
+          if(upgInfo) {
+            /// Добавляем подсказку по улучшению
+            $('<font title="' + upgInfo[1] + '">' + upgInfo[0] + '</font>')
+              .css({
+                color: '#900',
+                display: 'block',
+                'font-weight': 'bold',
+                position: 'absolute',
+                margin: '-62px 0 0 22px',
+                background: '#ecffed',
+                padding: '0 1px 0px'
+              })
+              .appendTo(this.parentNode);
           }
 
         });
